973a5f175be46b1720e8707749cda6115b4b06a7
JuanCa19 <cardenasrivera1927@gmail.com>
2025-11-24 07:35:59 -0500

    App Restaurante

---
+++ src/main/java/com/mycompany/apprestaurante/AppRestaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apprestaurante;
+
+/**
+ *
+ * @author carde
+ */
+public class AppRestaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
